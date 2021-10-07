5df334c3acf4e119a6a6c29e45aaf6a588dbc929
Thanhtrungg <thanhtrungpttd0@gmail.com>
2021-10-07 07:39:29 +0100

    FileNop

---
+++ src/com/trung2008110125/BTTuan4/CuTomer.java
@@ -0,0 +1,21 @@
+package com.trung2008110125.BTTuan4;
+import java.util.Scanner;
+public class CuTomer {
+    String name;
+    String address;
+    Scanner in = new Scanner(System.in);
+    void getName(){
+        System.out.print("Nhập tên khách hàng: ");
+        name = in.nextLine();
+
+    }
+    void getAddress(){
+        address = in.nextLine();
+        System.out.print("Nhập địa chỉ khách hàng: ");
+    }
+    void show(){
+        System.out.println("Tên khách hàng: "+name);
+        System.out.println("Địa chỉ khách hàng: "+address);
+    }
+
+}
