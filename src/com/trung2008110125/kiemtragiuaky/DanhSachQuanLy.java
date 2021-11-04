23da52a4fda3599d1b5db8a35764d61b675bf754
Thanhtrungg <thanhtrungpttd0@gmail.com>
2021-11-04 09:04:29 +0700

    kiemtragiuaky

---
+++ src/com/trung2008110125/kiemtragiuaky/DanhSachQuanLy.java
@@ -0,0 +1,10 @@
+package com.trung2008110125.kiemtragiuaky;
+import java.util.ArrayList;
+import java.util.List;
+public class DanhSachQuanLy {
+    public static void main(String[] args) {
+    List<String> danhSach;
+    danhSach = new ArrayList<>();
+
+}
+}
